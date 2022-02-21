498fbf13e11f0593a896a176df97e9dfad5de690
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-21 17:11:49 -0500

    Separate add book form in another component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/books.scss';
+import AddBook from './AddBook';
 import Book from './Book';
 
 const Books = () => {
@@ -15,21 +16,7 @@ const Books = () => {
         <Book title={test.title} category={test.category} />
         <Book title={test.title} category={test.category} />
       </ul>
-      <form>
-        <label htmlFor="book-title">
-          <input id="book-title" name="bookTitle" type="text" placeholder="Book Title" />
-        </label>
-        <label htmlFor="category-select">
-          <select id="category-select" name="category">
-            <option disabled selected> Category </option>
-            <option value="fiction"> Fiction </option>
-            <option value="action"> Action </option>
-            <option value="suspense"> Suspense </option>
-            <option value="Romance"> Romance </option>
-          </select>
-        </label>
-        <button type="submit"> Add Book </button>
-      </form>
+      <AddBook />
     </div>
   );
 };
