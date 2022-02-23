cd567ecb07ecc7f32ec7eccee188f7b7e3e41752
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-23 10:52:54 -0500

    Add onChange handler for AddBook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddBook = () => (
-  <form>
-    <label htmlFor="book-title">
-      <input id="book-title" name="bookTitle" type="text" placeholder="Book Title" />
-    </label>
-    <label htmlFor="category-select">
-      <select id="category-select" name="category">
-        <option disabled selected> Category </option>
-        <option value="fiction"> Fiction </option>
-        <option value="action"> Action </option>
-        <option value="suspense"> Suspense </option>
-        <option value="Romance"> Romance </option>
-      </select>
-    </label>
-    <button type="submit"> Add Book </button>
-  </form>
-);
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Category');
+
+  const onChangeHandler = (event) => {
+    const nodeName = event.target.name;
+    if (nodeName === 'bookTitle') {
+      setTitle(event.target.value);
+      return;
+    }
+    if (nodeName === 'bookAuthor') {
+      setAuthor(event.target.value);
+      return;
+    }
+    if (nodeName === 'category') {
+      setCategory(event.target.value);
+    }
+  };
+
+  return (
+    <form>
+      <label htmlFor="book-title">
+        <input onChange={onChangeHandler} id="book-title" name="bookTitle" type="text" placeholder="Book Title" value={title} required />
+      </label>
+      <label htmlFor="book-author">
+        <input onChange={onChangeHandler} id="book-author" name="bookAuthor" type="text" placeholder="Author" value={author} required />
+      </label>
+      <label htmlFor="category-select">
+        <select onChange={onChangeHandler} id="category-select" name="category" value={category} required>
+          <option disabled selected> Category </option>
+          <option value="fiction"> Fiction </option>
+          <option value="action"> Action </option>
+          <option value="suspense"> Suspense </option>
+          <option value="Romance"> Romance </option>
+        </select>
+      </label>
+      <button type="submit"> Add Book </button>
+    </form>
+  );
+};
 
 export default AddBook;
