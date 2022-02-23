7816c5b3668d4f6f8ccb086dae6e9b168c2a7b7e
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-23 11:26:53 -0500

    Implement remove from store functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
   const {
@@ -9,13 +11,19 @@ const Book = (props) => {
     category,
   } = props;
 
+  const dispatch = useDispatch();
+
+  const removeSelf = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <li id={id} className="book">
       <div className="info">
         <p>{category}</p>
         <h2>{title}</h2>
         <p>{author}</p>
-        <button type="button">
+        <button type="button" onClick={removeSelf}>
           Remove
         </button>
       </div>
