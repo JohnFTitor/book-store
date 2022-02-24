abf12e4f82de9d1d998e0444dba7eebf94d4524d
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-24 16:21:11 -0500

    Add interval css functionality

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,18 +5,37 @@ import '../styles/progressBar.scss';
 const ProgressBar = (props) => {
   const [count, setCount] = useState(0);
   const [intervalId, setIntervalId] = useState(null);
+  const [leftRotationValue, setLeftRotation] = useState(0);
+  const [rightRotationValue, setRightRotation] = useState(0);
   const { limit } = props;
 
   const updateProgress = () => {
+    let innerCount = 0;
     const id = setInterval(() => {
       setCount((count) => count + 1);
-    }, 40);
+      innerCount += 1;
+      if (innerCount <= 50) {
+        setLeftRotation((leftRotationValue) => leftRotationValue + 3.6);
+      } else {
+        setRightRotation((rightRotationValue) => rightRotationValue + 3.6);
+      }
+    }, 20);
     setIntervalId(id);
   };
 
   if (count === limit) {
     clearInterval(intervalId);
   }
+
+  const leftRotation = {
+    transform: `rotate(${leftRotationValue}deg)`,
+    transition: 'all 0.2s ease-in',
+  };
+
+  const rightRotation = {
+    transform: `rotate(${rightRotationValue}deg)`,
+    transition: 'all 0.2s ease-in',
+  };
 
   useEffect(updateProgress, []);
 
@@ -26,10 +45,10 @@ const ProgressBar = (props) => {
         <div className="inner" />
         <div className="circle">
           <div className="bar left">
-            <div className="progress" />
+            <div style={leftRotation} className="progress" />
           </div>
           <div className="bar right">
-            <div className="progress" />
+            <div style={rightRotation} className="progress" />
           </div>
         </div>
       </div>
